551cf50f525f29c80b54350ec49cead363c77bc9
miguelybera <2018115528@ust-ics.mygbiz.com>
2020-03-01 00:11:09 +0800

    second commit

--- form.js
+++ form.js
@@ -1,35 +1,65 @@
-function check() {
-    var inputVal = document.getElementById("LoginUserName");
-    var inputVal1 = document.getElementById("Pword");
-    var inputVal2 = document.getElementById("CPword");
-    var inputVal3 = document.getElementById("Email");
-    
-    if (inputVal.value == "") {
-        inputVal.style.backgroundColor = "Red";
-    }
-    else{
-        inputVal.style.backgroundColor = "";
-    }
 
-    if (inputVal1.value == ""||inputVal1.value!=inputVal2.value) {
-        inputVal1.style.backgroundColor = "Red";
-    }else if (inputVal1.value==inputVal2.value) {
-        inputVal1.style.backgroundColor = "Green";}
-    else{
-        inputVal1.style.backgroundColor = "";
-    }if (inputVal2.value == ""||inputVal1.value!=inputVal2.value) {
-        inputVal2.style.backgroundColor = "Red";
+function checkpass(){
+var PLength = document.getElementById("Pword").value.length
+if(PLength>6){
+    document.getElementById("LengthChecker").innerHTML="Strong"
+}else{
+    document.getElementById("LengthChecker").innerHTML="Weak"
+}
+}
+
+function username(){
+
+if(document.getElementById("LoginUserName").value==""){
+    document.getElementById("LoginUserName").style.backgroundColor="Red";
+    document.getElementById("validation").innerHTML="Please Input UserName <br>";
+    
+    
+}else{
+    document.getElementById("LoginUserName").style.backgroundColor="Green";
+    document.getElementById("validation").innerHTML="";
+}
+}
+function passcode(){
+    if(document.getElementById("Pword").value==""){
+        document.getElementById("Pword").style.backgroundColor="Red";
+        document.getElementById("validation").innerHTML+="Please Input Password <br>";
+       
+    
+}else{
+    document.getElementById("Pword").style.backgroundColor="Green";
+    document.getElementById("validation").innerHTML+="";
+        
+}
+}
+function Cpasscode(){
+    if (document.getElementById("CPword").value==""||document.getElementById("CPword").value!=document.getElementById("Pword").value){
+        document.getElementById("CPword").style.backgroundColor="Red";
+        document.getElementById("validation").innerHTML+="Passwords are not the same <br>";
+        
+    }else{
+        document.getElementById("CPword").style.backgroundColor="Green";
+        document.getElementById("validation").innerHTML+="";
     }
-    else if (inputVal1.value==inputVal2.value) {
-        inputVal2.style.backgroundColor = "Green";
-    }
-    else{
-        inputVal2.style.backgroundColor = "";
-    }
-    if (inputVal3.value == "") {
-        inputVal3.style.backgroundColor = "Red";
-    }
-    else{
-        inputVal3.style.backgroundColor = "";
-    }
+}
+function validateEmail(){
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (document.getElementById("Email").value.match(mailformat)){
+    document.getElementById("Email").style.backgroundColor="Green";
+    document.getElementById("validation").innerHTML+="";
+}else{
+    document.getElementById("Email").style.backgroundColor="Red";
+    document.getElementById("validation").innerHTML+="Mail is wrong";
+  
+}
+}
+
+
+function check(){
+username();
+passcode();
+Cpasscode();
+validateEmail();
+
+ 
 }
